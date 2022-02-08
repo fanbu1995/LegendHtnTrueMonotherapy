5a9fcc1975f2dae3004c958141fee87fbfd3c2cb
Admin_FBu2 <Admin_FBu2@its.jnj.com>
2022-02-08 15:46:26 -0500

    update Redshift run code

--- extras/CodeToRunRedShift.R
+++ extras/CodeToRunRedShift.R
@@ -1,19 +1,26 @@
 
+library(LEGENDHTNTrueMonotherapy)
 
+# Optional: specify where the temporary files (used by the Andromeda package) will be created:
+options(andromedaTempFolder = "d:/andromedaTemp")
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores()
+
+# specify where the Drivers are
 Sys.setenv(DATABASECONNECTOR_JAR_FOLDER='D:/Drivers')
 
-## Optum DoD
-
+## 1. Optum DoD------------
 cdmDatabaseSchema <- "cdm_optum_extended_dod_v1825" #"cdm_truven_ccae_v1709" #"cdm_idm_ccae_seta"
 serverSuffix <-"optum_extended_dod" #"truven_ccae" # "ibm"
 cohortDatabaseSchema <- "scratch_fbu2"
-databaseId<- "CCAE"
-databaseName <- "IBM Health MarketScan® Commercial Claims and Encounters"
-databaseDescription <- "IBM MarketScan® Commercial Claims and Encounters (CCAE) adjudicated US health insurance claims for Medicaid enrollees from multiple states and includes hospital discharge diagnoses, outpatient diagnoses and procedures, and outpatient pharmacy claims as well as ethnicity and Medicare eligibility. Members maintain their same identifier even if they leave the system for a brief period however the dataset lacks lab data."
+databaseId <- "OptumDod"
+databaseName <- "Optum Clinformatics Extended Data Mart - Date of Death (DOD)"
+databaseDescription <- "Optum Clinformatics Extended DataMart is an adjudicated US administrative health claims database for members of private health insurance, who are fully insured in commercial plans or in administrative services only (ASOs), Legacy Medicare Choice Lives (prior to January 2006), and Medicare Advantage (Medicare Advantage Prescription Drug coverage starting January 2006).  The population is primarily representative of commercial claims patients (0-65 years old) with some Medicare (65+ years old) however ages are capped at 90 years.  It includes data captured from administrative claims processed from inpatient and outpatient medical services and prescriptions as dispensed, as well as results for outpatient lab tests processed by large national lab vendors who participate in data exchange with Optum.  This dataset also provides date of death (month and year only) for members with both medical and pharmacy coverage from the Social Security Death Master File (however after 2011 reporting frequency changed due to changes in reporting requirements) and location information for patients is at the US state level."
 tablePrefix <- "legend_t2dm_ccae"
 outputFolder <- "d:/LegendMonotherapy_OptumDOD" # DONE
 
-## IBM CCAE
+## 3. IBM CCAE ------------
 cdmDatabaseSchema <- "cdm_truven_ccae_v1709" #"cdm_idm_ccae_seta"
 serverSuffix <- "truven_ccae" # "ibm"
 cohortDatabaseSchema <- "scratch_fbu2"
@@ -23,6 +30,7 @@ databaseDescription <- "IBM MarketScan® Commercial Claims and Encounters (CCAE)
 tablePrefix <- "legend_monotherapy_ccae"
 outputFolder <- "d:/LegendMonotherapy_ccae" # DONE
 
+## fill out connection details ------------
 conn <- DatabaseConnector::createConnectionDetails(
   dbms = "redshift",
   server = paste0(keyring::key_get("epi_server"), "/", !!serverSuffix),
